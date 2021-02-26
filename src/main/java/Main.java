a4630a720d1befade562c6b58d31bc627e3e8c68
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2021-02-26 16:41:04 -0700

    Let's not repeat too many string constants.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,15 @@
 public class Main {
 	
+	public static final String ADD = "add";
+	public static final String SUBTRACT = "subtract";
+	public static final String MULTIPLY = "multiply";
+	public static final String DIVIDE = "divide";
+	public static final String FIND_FIBONACCI = "fib";
+	public static final String CONVERT_TO_BINARY = "binary";
+	
 	public static void main(String[] args) {
 		if (args.length < 1) {
-			System.out.println("Please specify an operation: add | subtract | multiply | divide | fib | binary");
+			System.out.println("Please specify an operation: " + ADD + " | " + SUBTRACT + " | " + MULTIPLY + " | " + DIVIDE + " | " + FIND_FIBONACCI + " | " + CONVERT_TO_BINARY);
 			return;
 		}
 		
@@ -10,7 +17,7 @@ public class Main {
 		String operation = args[0];
 		String result;
 		
-		if (operation.equals("add")) {
+		if (operation.equals(ADD)) {
 			if (args.length < 3) {
 				System.out.println("Too few arguments to add. Need two.");
 				return;
@@ -19,7 +26,7 @@ public class Main {
 			int b = Integer.parseInt(args[2]);
 			result = String.valueOf(calculator.add(a, b));
 			
-		} else if (operation.equals("subtract")) {
+		} else if (operation.equals(SUBTRACT)) {
 			if (args.length < 3) {
 				System.out.println("Too few arguments to subtract. Need two.");
 				return;
@@ -28,7 +35,7 @@ public class Main {
 			int b = Integer.parseInt(args[2]);
 			result = String.valueOf(calculator.subtract(a, b));
 			
-		} else if (operation.equals("multiply")) {
+		} else if (operation.equals(MULTIPLY)) {
 			if (args.length < 3) {
 				System.out.println("Too few arguments to multiply. Need two.");
 				return;
@@ -37,7 +44,7 @@ public class Main {
 			int b = Integer.parseInt(args[2]);
 			result = String.valueOf(calculator.multiply(a, b));
 			
-		} else if (operation.equals("divide")) {
+		} else if (operation.equals(DIVIDE)) {
 			if (args.length < 3) {
 				System.out.println("Too few arguments to divide. Need two.");
 				return;
@@ -46,17 +53,17 @@ public class Main {
 			int b = Integer.parseInt(args[2]);
 			result = String.valueOf(calculator.divide(a, b));
 			
-		} else if (operation.equals("fib")) {
+		} else if (operation.equals(FIND_FIBONACCI)) {
 			if (args.length < 2) {
-				System.out.println("Need one integer input.");
+				System.out.println("Need one integer input to find Fibonacci.");
 				return;
 			}
 			int n = Integer.parseInt(args[1]);
 			result = String.valueOf(calculator.fibonacciNumberFinder(n));
 			
-		} else if (operation.equals("binary")) {
+		} else if (operation.equals(CONVERT_TO_BINARY)) {
 			if (args.length < 2) {
-				System.out.println("Need one integer input.");
+				System.out.println("Need one integer input to convert to binary.");
 				return;
 			}
 			int n = Integer.parseInt(args[1]);
