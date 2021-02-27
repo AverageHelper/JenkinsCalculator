a14ec4a379facda3da62995074d55afc8f659720
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2021-02-26 17:12:52 -0700

    Make our interface an interactive one

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import javax.transaction.InvalidTransactionException;
+import java.util.Scanner;
+
 public class Main {
 	
 	public static final String ADD = "add";
@@ -6,75 +9,104 @@ public class Main {
 	public static final String DIVIDE = "divide";
 	public static final String FIND_FIBONACCI = "fib";
 	public static final String CONVERT_TO_BINARY = "binary";
+	public static final String EXIT = "exit";
 	
-	public static void main(String[] args) {
-		if (args.length < 1) {
-			System.out.println("Please specify an operation: " + ADD + " | " + SUBTRACT + " | " + MULTIPLY + " | " + DIVIDE + " | " + FIND_FIBONACCI + " | " + CONVERT_TO_BINARY);
-			return;
-		}
-		
-		Calculator calculator = new Calculator();
-		String operation = args[0];
-		String result;
+	private static final String LEAVE_MSG = "Goodbye!";
+	
+	private static String runOperation(Calculator calculator, String operation, String[] args) throws NoSuchMethodException, InvalidTransactionException {
+		// Switch over the provided operation and do the thing
 		
 		if (operation.equals(ADD)) {
 			if (args.length < 3) {
-				System.out.println("Too few arguments to add. Need two.");
-				return;
+				throw new InvalidTransactionException("Too few arguments to add. Need two.");
 			}
 			int a = Integer.parseInt(args[1]);
 			int b = Integer.parseInt(args[2]);
-			result = String.valueOf(calculator.add(a, b));
+			return String.valueOf(calculator.add(a, b));
 			
 		} else if (operation.equals(SUBTRACT)) {
 			if (args.length < 3) {
-				System.out.println("Too few arguments to subtract. Need two.");
-				return;
+				throw new InvalidTransactionException("Too few arguments to subtract. Need two.");
 			}
 			int a = Integer.parseInt(args[1]);
 			int b = Integer.parseInt(args[2]);
-			result = String.valueOf(calculator.subtract(a, b));
+			return String.valueOf(calculator.subtract(a, b));
 			
 		} else if (operation.equals(MULTIPLY)) {
 			if (args.length < 3) {
-				System.out.println("Too few arguments to multiply. Need two.");
-				return;
+				throw new InvalidTransactionException("Too few arguments to multiply. Need two.");
 			}
 			int a = Integer.parseInt(args[1]);
 			int b = Integer.parseInt(args[2]);
-			result = String.valueOf(calculator.multiply(a, b));
+			return String.valueOf(calculator.multiply(a, b));
 			
 		} else if (operation.equals(DIVIDE)) {
 			if (args.length < 3) {
-				System.out.println("Too few arguments to divide. Need two.");
-				return;
+				throw new InvalidTransactionException("Too few arguments to divide. Need two.");
 			}
 			int a = Integer.parseInt(args[1]);
 			int b = Integer.parseInt(args[2]);
-			result = String.valueOf(calculator.divide(a, b));
+			return String.valueOf(calculator.divide(a, b));
 			
 		} else if (operation.equals(FIND_FIBONACCI)) {
 			if (args.length < 2) {
-				System.out.println("Need one integer input to find Fibonacci.");
-				return;
+				throw new InvalidTransactionException("Need one integer input to find Fibonacci.");
 			}
 			int n = Integer.parseInt(args[1]);
-			result = String.valueOf(calculator.fibonacciNumberFinder(n));
+			return String.valueOf(calculator.fibonacciNumberFinder(n));
 			
 		} else if (operation.equals(CONVERT_TO_BINARY)) {
 			if (args.length < 2) {
-				System.out.println("Need one integer input to convert to binary.");
-				return;
+				throw new InvalidTransactionException("Need one integer input to convert to binary.");
 			}
 			int n = Integer.parseInt(args[1]);
-			result = String.valueOf(calculator.intToBinaryNumber(n));
+			return String.valueOf(calculator.intToBinaryNumber(n));
+			
+		} else if (operation.equals(EXIT)) {
+			return LEAVE_MSG;
 			
 		} else {
-			System.out.println("Invalid operation: " + operation);
-			return;
+			throw new NoSuchMethodException(operation);
 		}
-		
-		System.out.println(result);
+	}
+	
+	private static void handleInput(Scanner scanner) {
+		while (true) {
+			System.out.print("Enter a command: ");
+			String input = scanner.nextLine();
+			String[] args = input.split("\\s+");
+			
+			if (args.length < 1) {
+				System.out.println("Please specify an operation: " + ADD + " | " + SUBTRACT + " | " + MULTIPLY + " | " + DIVIDE + " | " + FIND_FIBONACCI + " | " + CONVERT_TO_BINARY + " | " + EXIT);
+				break;
+			}
+			
+			Calculator calculator = new Calculator();
+			String operation = args[0];
+			
+			try {
+				String result = runOperation(
+					calculator,
+					operation.toLowerCase(),
+					args
+				);
+				System.out.println(result);
+				if (result.equals(LEAVE_MSG)) {
+					break;
+				}
+				
+			} catch (NoSuchMethodException e) {
+				System.out.println("Invalid operation: " + e.getMessage());
+				
+			} catch (InvalidTransactionException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		handleInput(scanner);
 	}
 	
 }
